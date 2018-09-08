5e3ee5d378847796794ac47a6ae15317f1d1182e
surunzi <surunzi@foxmail.com>
2018-09-08 11:57:09 +0800

    Add prettier

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-module.exports = function (eruda) 
-{
-    let {evalCss} = eruda.util;
+module.exports = function(eruda) {
+  let { evalCss } = eruda.util
 
-    class Vue extends eruda.Tool {
-        constructor() {
-            super();
-            this.name = 'vue';
-            this._style = evalCss(require('./style.scss'));
-        }
-        init($el, container) 
-        {
-            super.init($el, container);
-            $el.html(require('./template.hbs')());
-        }
-        show() 
-        {
-            super.show();
-        }
-        hide()
-        {
-            super.hide();
-        }
-        destroy() 
-        {
-            super.destroy();
-            evalCss.remove(this._style);
-        }
+  class Vue extends eruda.Tool {
+    constructor() {
+      super()
+      this.name = 'vue'
+      this._style = evalCss(require('./style.scss'))
     }
+    init($el, container) {
+      super.init($el, container)
+      $el.html(require('./template.hbs')())
+    }
+    show() {
+      super.show()
+    }
+    hide() {
+      super.hide()
+    }
+    destroy() {
+      super.destroy()
+      evalCss.remove(this._style)
+    }
+  }
 
-    return new Vue();
-};
+  return new Vue()
+}
